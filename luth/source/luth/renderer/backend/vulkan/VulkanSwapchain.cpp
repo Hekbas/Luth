31d96282cf08d012202a36311047b9dd555b5a6b
hekbas <hekbas@gmail.com>
2026-01-10 01:56:28 +0100

    fix(renderer): use UNORM swapchain format to fix UI gamma

--- luth/source/luth/renderer/backend/vulkan/VulkanSwapchain.cpp
+++ luth/source/luth/renderer/backend/vulkan/VulkanSwapchain.cpp
@@ -83,10 +83,10 @@ namespace Luth
         std::vector<VkPresentModeKHR> presentModes(presentModeCount);
         vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice, m_Surface, &presentModeCount, presentModes.data());
 
-        // Choose Format (Prefer B8G8R8A8 SRGB)
+        // Choose Format (Prefer B8G8R8A8 UNORM to avoid double gamma correction on UI)
         VkSurfaceFormatKHR surfaceFormat = formats[0];
         for (const auto& availableFormat : formats) {
-            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && 
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && 
                 availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
                 surfaceFormat = availableFormat;
                 break;
